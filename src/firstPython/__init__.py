bdc3a13912b58ef758b4329d829c603ccda088c4
baibuchuanyang <yangchuanbubai@163.com>
2013-10-13 22:55:21 +0800

    first test git

---
+++ src/firstPython/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#-*-coding:utf8-*-
+s1=input("Inputyourname:")
+s2='asdf'
+print (s1+'say:asdfs 办')
+print  ('say:asdf')
---
+++ src/firstPython/test.py
@@ -0,0 +1,11 @@
+'''
+Created on 2013-4-23
+
+@author: Administrator
+'''
+
+
+s1=input("Inputyourname:")
+s2='asdf'
+print (s1+'say:asdfs 办')
+print  ('say:asdf')
